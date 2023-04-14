9eb5cd64f8c9e349a60185c92e4e1f014d54458c
Kulwant Singh <ikulwant@amazon.com>
2023-04-14 09:27:27 -0700

    Add named pipe for reading output from vpc connector binary
    1. Added creating named pipe.
    2. Added listening and reading from named pipe.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 VPC CNI plugins for Amazon ECS and Amazon EKS.
 
+# Pre-requisites for running CNI plugin for EKS Windows
+1. Env variable  `AWS_VPC_CNI_K8S_CONNECTOR_BINARY_PATH` is required to be set. This will be already set in EKS Windows Optimized AMIs.
+Set env variable `AWS_VPC_CNI_K8S_CONNECTOR_BINARY_PATH` to `C:\Program Files\Amazon\EKS\cni\aws-vpc-cni-k8s-connector.exe`.
+
 ## Security disclosures
 
 If you think you’ve found a potential security issue, please do not post it in the Issues. Instead, please follow the instructions [here](https://aws.amazon.com/security/vulnerability-reporting/) or [email AWS security directly](mailto:aws-security@amazon.com).
--- plugins/vpc-bridge/config/k8s_connector.go
+++ plugins/vpc-bridge/config/k8s_connector.go
@@ -12,17 +12,35 @@
 // permissions and limitations under the License.
 
 //go:build enablek8sconnector
-// +build enablek8sconnector
 
 package config
 
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/aws/amazon-vpc-cni-plugins/network/vpc"
+
+	winio "github.com/Microsoft/go-winio"
+	"github.com/Microsoft/go-winio/pkg/guid"
+	log "github.com/cihub/seelog"
+)
+
+// Represents output of k8s connector received from named pipe
+type k8sExecutionResult struct {
+	stdout string // Represents message with required data.
+	stderr error  // Represents error while getting output.
+}
+
+const (
+	// Represents timeout for reading over results channel.
+	// Represents timeout for reading from connection object.
+	k8sExecutionTimeout = 3 * time.Minute
 )
 
 func init() {
@@ -34,12 +52,13 @@ func retrievePodConfig(netConfig *NetConfig) error {
 
 	kc := netConfig.Kubernetes
 
-	// retrieve Pod IP Address CIDR string using podName, podNamespace
-	ipAddress, err := retrievePodIPWithK8sConnector(kc.PodName, kc.Namespace)
+	// retrieve Pod IP Address CIDR string using podName, podNamespace.
+	ipAddress, err := retrievePodIPWithK8sConnector(kc.Namespace, kc.PodName)
 	if err != nil {
 		return fmt.Errorf("failed to get pod IP %s: %w", kc.PodName, err)
 	}
 
+	// Parse IP address returned by k8s binary.
 	ipAddr, err := vpc.GetIPAddressFromString(ipAddress)
 	if err != nil {
 		return fmt.Errorf("invalid IPAddress %s from pod label", ipAddress)
@@ -49,13 +68,57 @@ func retrievePodConfig(netConfig *NetConfig) error {
 	return nil
 }
 
-// retrievePodIPWithK8sConnector retrieves Pod IP using k8s connector binary.
-func retrievePodIPWithK8sConnector(podName, podNamespace string) (string, error) {
-	// Get Path to aws-vpc-cni-k8s-connector binary
-	connectorBinaryPath := os.Getenv("AWS_VPC_CNI_K8S_CONNECTOR_BINARY_PATH")
+// retrievePodIPWithK8sConnector retrieves Pod IP with k8s connector binary using named pipe.
+func retrievePodIPWithK8sConnector(podNamespace, podName string) (string, error) {
 
-	// Prepare command to execute binary with required args
-	cmd := exec.Command(connectorBinaryPath, "-pod-name", podName, "-pod-namespace", podNamespace)
+	// Create named pipe, used to get output from k8s connector binary.
+	// pipeListener will accept connections in separate go routine. Output of binary is received via channel.
+	pipeListener, executionResultChan, err := createNamePipeForOutputFromK8sConnector()
+	if err != nil {
+		return "", fmt.Errorf("retrievePodIPWithK8sConnector: error creating named pipe %w", err)
+	}
+	defer pipeListener.Close()       // Defer closing named pipe listener.
+	defer close(executionResultChan) // Defer closing result channel.
+
+	namedPipePath := getPipeName(pipeListener)
+	log.Debugf("named piped: %s created", namedPipePath)
+
+	// Execute k8s connector binary with pod namespace, pod name and named pipe path.
+	err = executeK8sConnector(podNamespace, podName, namedPipePath)
+	if err != nil {
+		return "", fmt.Errorf("retrievePodIPWithK8sConnector: error executing k8s connector %w", err)
+	}
+
+	// Read output of k8s binary from named pipe.
+	var result k8sExecutionResult
+	select {
+	case result = <-executionResultChan:
+	case <-time.After(k8sExecutionTimeout):
+		return "", fmt.Errorf("retrievePodIPWithK8sConnector: timeout getting output of k8s connector")
+	}
+
+	if result.stderr != nil {
+		return "", fmt.Errorf("retrievePodIPWithK8sConnector: error in k8s connector %w", result.stderr)
+	}
+	podIP := result.stdout
+	log.Debugf("got pod IP: %s for pod %s in namespace %s", podIP, podName, podNamespace)
+
+	return podIP, nil
+}
+
+// executeK8sConnector executes aws-vpc-cni-k8s-connector binary and get pod IP over named pipe.
+// Execution logs from binary are stderr.
+func executeK8sConnector(podNamespace, podName, pipe string) error {
+	// Get Path to aws-vpc-cni-k8s-connector binary from env variable.
+	connectorBinaryPath := os.Getenv("AWS_VPC_CNI_K8S_CONNECTOR_BINARY_PATH")
+	// Use VPC CNI log level for k8s connector binary.
+	binaryLogLevel := os.Getenv("VPC_CNI_LOG_LEVEL")
+
+	// Prepare command to execute binary with required args.
+	cmd := exec.Command(connectorBinaryPath,
+		"-pod-name", podName, "-pod-namespace", podNamespace,
+		"-pipe", pipe, "-log-level", binaryLogLevel)
+	log.Debugf("Executing cmd: %s to get pod IP", cmd.String())
 
 	var errBytes, outputBytes bytes.Buffer
 	cmd.Stderr = &errBytes
@@ -63,14 +126,84 @@ func retrievePodIPWithK8sConnector(podName, podNamespace string) (string, error)
 
 	// Run command
 	err := cmd.Run()
+	if err != nil || cmd.ProcessState.ExitCode() != 0 {
+		return fmt.Errorf("executeK8sConnector: error running connector binary %w with error: %s", err, errBytes.String())
+	}
+
+	log.Infof("logs from k8s connector binary...\n%s ...end of k8s connector binary logs", errBytes.String())
+
+	return nil
+}
+
+// createNamePipeForOutputFromK8sConnector creates named pipe.
+// Starts listening and reading named pipe in go routine.
+// Returns listener for named pipe and channel for receiving output from channel.
+func createNamePipeForOutputFromK8sConnector() (net.Listener, chan k8sExecutionResult, error) {
+	// Generate GUID random string.
+	g, err := guid.NewV4()
 	if err != nil {
-		return "", fmt.Errorf("retrievePodIPWithK8sConnector: error running connector %w", err)
+		return nil, nil, fmt.Errorf("createNamePipeForOutputFromK8sConnector: error creating unique name for named pipe %w", err)
+	}
+	// Pipe name with k8s connector prefix.
+	pipeName := fmt.Sprintf(`\\.\pipe\aws-vpc-k8s-connector-%s`, g.String())
+
+	// Create pipe and get listener.
+	// listener is closed in main routine, to better handle timeout case.
+	pipeListener, err := winio.ListenPipe(pipeName, nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("createNamePipeForOutputFromK8sConnector: error creating named pipe:%s %w", pipeName, err)
 	}
 
-	// Return error if exit code is other than 0
-	if cmd.ProcessState.ExitCode() != 0 {
-		return "", fmt.Errorf("retrievePodIPWithK8sConnector: failure in connector %s", errBytes.String())
+	// Creating result channel to get k8s connector output.
+	result := make(chan k8sExecutionResult)
+
+	// Accept connections and read pipe in go routine as these operations are blocking.
+	go readFromNamePipe(pipeListener, result)
+
+	return pipeListener, result, nil
+}
+
+// getPipeName get name/path for named pipe using pipe listener.
+func getPipeName(listener net.Listener) string {
+	return listener.Addr().String()
+}
+
+// readFromNamePipe start accepting connections and read message from named pipe.
+// This runs in go routine as Accept and Read from conn as blocking operations.
+// Result is sent over result channel.
+// Timeout for read operation on conn.
+func readFromNamePipe(pipeListener net.Listener, result chan k8sExecutionResult) {
+	// Get named pipe path
+	pipNamePath := getPipeName(pipeListener)
+
+	// Accept connection on named pipe. This operation is blocking until client dials into pipe.
+	conn, err := pipeListener.Accept()
+	if err != nil {
+		// Send error over channel
+		result <- k8sExecutionResult{stderr: fmt.Errorf("readFromNamePipe: error accepting connection on pipe listener %v %w", pipNamePath, err)}
+		return
+	}
+	defer conn.Close() // Defer closing connection.
+
+	// Setup read timeout on conn object.
+	err = conn.SetReadDeadline(time.Now().Add(3 * time.Minute))
+	if err != nil {
+		// Send error over channel
+		result <- k8sExecutionResult{stderr: fmt.Errorf("readFromNamePipe: error setting timeout for pipe connection %v %w", pipNamePath, err)}
+		return
 	}
 
-	return outputBytes.String(), nil
+	// Get message from named pipe. This operation is blocking until client has written a message.
+	// Returns error if message not available within above given timeout.
+	var output bytes.Buffer
+	_, err = io.Copy(&output, conn) // Copy message from pipe connection to buffer.
+	if err != nil {
+		// Send error over channel
+		result <- k8sExecutionResult{stderr: fmt.Errorf("readFromNamePipe: error reading from named piped %v %w", pipNamePath, err)}
+		return
+	}
+
+	// Send output from named pipe on channel
+	result <- k8sExecutionResult{stdout: output.String()}
+	return
 }
